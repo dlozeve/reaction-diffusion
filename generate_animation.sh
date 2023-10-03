c607954d344617b08238113e6be915827d52ee9a
Dimitri Lozeve <dimitri@lozeve.com>
2023-10-03 13:38:33 +0200

    Add reaction-diffusion in BQN, with video generation script

---
+++ generate_animation.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+mkdir -p out
+bqn grayscott.bqn
+# first image is often bad
+rm out/step00000.pnm
+# convert all to PNG
+parallel --jobs 8 "pnmtopng {} > {.}.png" ::: out/*.pnm
+# generate video
+ffmpeg -y -framerate 30 -pattern_type glob -i 'out/*.png' -r 30 -vf scale=500:-1 -loop 0 out.mp4
